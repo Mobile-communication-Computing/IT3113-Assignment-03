ad9c8676252fc8fccb3fff1a04999c354d278809
piumal302 <piumal302@gmail.com>
2025-01-06 22:55:29 +0530

    Update Courses.js
    StyleSheet

--- components/Courses.js
+++ components/Courses.js
@@ -5,3 +5,26 @@ import { courses } from "../data/StudentsDb";
 
 const Courses = ({}) => {};
 export default Courses;
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+  },
+  h1: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 4,
+    textAlign: "center",
+  },
+  image: {
+    marginTop: 20,
+    width: "100%",
+    height: 100,
+    resizeMode: "contain",
+  },
+  profile_pic: {
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+    margin: "auto",
+  },
+});

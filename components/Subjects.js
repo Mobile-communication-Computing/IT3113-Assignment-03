f5b59cfb23e38ffa21e9a5f92d977ba1b584f031
piumal302 <piumal302@gmail.com>
2025-01-06 22:27:16 +0530

    Update Subjects.js

---
+++ components/Subjects.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Card, DataTable } from "react-native-paper";
+import {
+  courses,
+  marks as marksData,
+  subjects as subjectsData,
+} from "../data/StudentsDb";
+
+const Subjects = ({ student }) => {};
+
+export default Subjects;

acf09802154036943d3601195b36299cd7320df6
piumal302 <piumal302@gmail.com>
2025-01-06 22:27:11 +0530

    Update Courses.js

---
+++ components/Courses.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Card } from "react-native-paper";
+import { courses } from "../data/StudentsDb";
+
+const Courses = ({}) => {};
+export default Courses;

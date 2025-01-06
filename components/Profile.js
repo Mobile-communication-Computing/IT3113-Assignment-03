31b9e6fd4a36f8ea9fb8fcf6101fd35fab94952e
piumal302 <piumal302@gmail.com>
2025-01-06 22:27:14 +0530

    Update Profile.js

---
+++ components/Profile.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Card } from "react-native-paper";
+
+const Profile = ({ student }) => {};
+export default Profile;
